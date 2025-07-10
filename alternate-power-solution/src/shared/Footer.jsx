1716f9acc0baa318b41ffae0c1807b5dc2a8169d
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-11 03:43:24 +0600

    Footer and Navbar added

--- alternate-power-solution/src/shared/Footer.jsx
+++ alternate-power-solution/src/shared/Footer.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaFacebook, FaGoogle, FaInstagramSquare, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { motion } from "motion/react"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <motion.footer
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ amount: 0.3 }}
+        className="footer bg-purple-950 text-white lg:flex lg:gap-44 pt-6 justify-center">
+        <aside>
+          <Link href="/">
+            <Image className="hidden lg:block" src="/logo.png" alt="Logo" width={100} height={60} />
+          </Link>
+          <p>
+            Privacy Policy Terms <br /> & Conditions <br />
+            Refund Policy <br />
 
-export default Footer
+          </p>
+          {/*Social Link*/}
+          <div className="flex space-x-4 mt-4">
+            <Link href={"https://www.google.com"}>
+              <FaGoogle className="text-xl text-blue-500" />
+            </Link>
+            <Link href={"https://www.facebook.com/profile.php?id=100007196595674"}>
+              <FaFacebook className="text-xl text-sky-400" />
+            </Link>
+            <Link href={"https://x.com/Nazrul211002144"}>
+              <FaTwitter className="text-xl" />
+            </Link>
+            <Link href={"https://www.instagram.com/mdnazrulmarine/?igsh=MmVlYXcwNXl0enI5#"}>
+              <FaInstagramSquare className="text-xl text-red-600" />
+            </Link>
+            <Link href={"https://www.linkedin.com/in/nazrul-islam-a6080730b"}>
+              <FaLinkedin className="text-xl text-sky-500" />
+            </Link>
+          </div>
+        </aside>
+        <nav>
+          <h6 className="footer-title text-xl font-bold">About</h6>
+          <Link href={"/"} className="link link-hover">
+            Home
+          </Link>
+          <Link href={"/services"} className="link link-hover">
+            Services
+          </Link>
+          <Link href={"/contact"} className="link link-hover">
+            Contact
+          </Link>
+        </nav>
+        <nav>
+          <h6 className="footer-title text-xl font-bold">Company</h6>
+          <Link href={"/"} className="link link-hover">Why Alternate Power Solutions</Link>
+          <Link href={"/team"} className="link link-hover">Meet Our Team</Link>
+          <Link href={"/about"} className="link link-hover">About</Link>
+        </nav>
+        <nav>
+          <h6 className="footer-title text-xl font-bold">Support</h6>
+          <Link href={'/'} className="link link-hover">Support Center</Link>
+          <Link href={'/'} className="link link-hover">Feedback</Link>
+          <Link href={'/'} className="link link-hover">Accessibility</Link>
+        </nav>
+      </motion.footer>
+    </div>
+  );
+};
+
+export default Footer;
--- alternate-power-solution/src/shared/Navbar.jsx
+++ alternate-power-solution/src/shared/Navbar.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { ModeToggle } from "../mode-toggoler";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const pathName = usePathname();
 
-export default Navbar
+
+  const [scrollY, setScrollY] = useState(0);
+
+   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleScroll = () => {
+        setScrollY(window.scrollY);
+      };
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+  
+
+  return (
+    <div className={`${scrollY > 100 ? 'bg-[#D1A054]' : 'bg-black/30'} transition-colors duration-500  shadow-xl fixed z-10 w-full top-0`}>
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              {links?.map((link) => (
+                <Link key={link.path} href={link.path}>
+                  {link.title}
+                </Link>
+              ))}
+            </ul>
+          </div>
+          <Link href="/">
+            <Image className="hidden lg:block" src="/logo.png" alt="Logo" width={100} height={60} />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex  ">
+          <ul className="menu menu-horizontal px-1 flex gap-4 text-xl font-bold ">
+            {links?.map((link) => (
+              <Link
+                className={` hover:text-primary duration-300 ${pathName == link.path ? "text-[#ff3811]" : scrollY > 100 ? "text-black" : "text-white"}`}
+                key={link.path}
+                href={link.path}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="navbar-end ">
+          <div className="flex space-x-3 items-center">
+            <div className="avatar">
+            </div>
+            <ModeToggle/>
+            <FaWhatsapp className="text-3xl text-green-700 font-bold" />
+            <Link href={"/login"}>
+              <h1 className="font-bold">Call Now: 068 319 3323</h1>
+              <h1 className="font-bold text-sky-700">Open 24/7, 365!</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const links = [
+  {
+    title: "Home",
+    path: "/home",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Services",
+    path: "/services",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Posts",
+    path: "/post",
+  },
+];
+
+export default Navbar;
