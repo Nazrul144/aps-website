daa33ab4348064e4db32e8117abadad5e975dbaa
Nazrul Islam <nazrulislam.cse28@gmail.com>
2025-07-11 04:18:03 +0600

    Radio groun button added

--- alternate-power-solution/src/app/page.js
+++ alternate-power-solution/src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
+
 import HomePage from "./home/page";
 
 export default function Home() {
   return (
    <main>
       <HomePage/>
+      <h1 className="mt-6 text-4xl text-red-300">Welcome to the Alternate Power Solution</h1>
    </main>
   );
 }
